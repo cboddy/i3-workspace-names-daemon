5bfb0c7670f191b48c8d542fd6ed168f088d3868
Chris Boddy <chris@boddy.im>
2018-11-25 20:06:59 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(name='i3-workspace-names-daemon',
-      version='0.1',
+      version='0.2',
       description='Dynamically update the name of each i3wm workspace using font-awesome icons or the names of applications running in each workspace.',
       long_description=long_description,
       url='https://github.com/cboddy/i3-workspace-names-daemon',
       license='MIT',
       zip_safe=False,
       py_modules=['i3_workspace_names_daemon', 'fa_icons'],
-      install_requires=requirements,
+      install_requires=["i3ipc"],
       author='Chris Boddy',
       author_email='chris@boddy.im',
       entry_points={
